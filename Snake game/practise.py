d05cf6696ea34e4931a8900bc31e27f1a46d78ab
ThejasRBharadwaj <165291598+ThejasRBharadwaj@users.noreply.github.com>
2026-03-17 16:45:42 +0530

    Add files via upload

--- Snake game/practise.py
+++ Snake game/practise.py
@@ -7,17 +7,29 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=("Times New Roman", 24, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=("Times New Roman", 24, "normal"))
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Times New Roman", 24, "normal"))    
-        
+        self.update_scoreboard()          
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
